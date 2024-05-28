1b50b01e421d9947010bd11b229521e1dd4c9132
redVELVET <rahmat.abdianto2@gmail.com>
2024-05-28 09:51:21 +0800

    membuat bottom navigation

--- app/src/main/java/com/dicoding/utaya/LoginActivity.kt
+++ app/src/main/java/com/dicoding/utaya/LoginActivity.kt
@@ -11,10 +11,6 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
     }
 }
