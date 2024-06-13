56767de152a3e089fa531e50dbd841868e789e57
redVELVET <rahmat.abdianto2@gmail.com>
2024-06-13 22:26:09 +0800

    menambahkan fungs_profile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation ("com.google.android.material:material:1.4.0")
+
 
 
     //camera
