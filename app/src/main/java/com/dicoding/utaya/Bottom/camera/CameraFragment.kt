5d28a8f780a528100747737eb940018570360659
redVELVET <rahmat.abdianto2@gmail.com>
2024-06-17 19:45:11 +0800

    menambahkan fungsi_profile

--- app/src/main/java/com/dicoding/utaya/Bottom/camera/CameraFragment.kt
+++ app/src/main/java/com/dicoding/utaya/Bottom/camera/CameraFragment.kt
@@ -3,31 +3,39 @@ package com.dicoding.utaya.Bottom.camera
 import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.dicoding.utaya.databinding.FragmentCameraBinding
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.Date
 
 class CameraFragment : Fragment() {
 
-    private var _binding : FragmentCameraBinding? = null
+    private var _binding: FragmentCameraBinding? = null
     private val binding get() = _binding!!
     private val REQUEST_IMAGE_CAPTURE = 1
     private val SELECT_PICTURE = 2
+    private lateinit var photoURI: Uri
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val CameraViewModel =
+        val cameraViewModel =
             ViewModelProvider(this).get(CameraViewModel::class.java)
-        _binding = FragmentCameraBinding.inflate(inflater,container,false)
-        val root : View = binding.root
+        _binding = FragmentCameraBinding.inflate(inflater, container, false)
+        val root: View = binding.root
 
         binding.cameraButton.setOnClickListener {
             dispatchTakePictureIntent()
@@ -43,7 +51,21 @@ class CameraFragment : Fragment() {
     private fun dispatchTakePictureIntent() {
         Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
             takePictureIntent.resolveActivity(requireActivity().packageManager)?.also {
-                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    // Error occurred while creating the File
+                    null
+                }
+                photoFile?.also {
+                    photoURI = FileProvider.getUriForFile(
+                        requireContext(),
+                        "com.dicoding.utaya.fileprovider",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                }
             }
         }
     }
@@ -57,13 +79,24 @@ class CameraFragment : Fragment() {
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
-            val imageBitmap = data?.extras?.get("data") as Bitmap
-            binding.previewImageView.setImageBitmap(imageBitmap)
+            binding.previewImageView.setImageURI(photoURI)
         } else if (requestCode == SELECT_PICTURE && resultCode == Activity.RESULT_OK) {
             val selectedImageUri = data?.data
             binding.previewImageView.setImageURI(selectedImageUri)
         }
+    }
+
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File = requireActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!
+        return File.createTempFile(
+            "JPEG_${timeStamp}_",
+            ".jpg",
+            storageDir
+        )
     }
 
     override fun onDestroyView() {
